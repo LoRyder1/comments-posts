91b89746b958e6f2c9e5191d715cb286aa55a209
Lovinder Pnag <lspnag@gmail.com>
2016-03-23 02:32:27 -0400

    add some Posts to init closure

--- grails-app/controllers/groovypublish/PostController.groovy
+++ grails-app/controllers/groovypublish/PostController.groovy
@@ -2,6 +2,23 @@ package groovypublish
 
 class PostController {
 
+  def defaultAction = 'list'
+
+  def edit = {
+    def post = Post.get(params.id)
+    if(!post) {
+      post = new Post()
+    }
+    render(view: 'edit', model:[post:post])
+  }
+
+  def list = {
+    render( view: 'list',
+            model:[posts:Post.list(
+                    sort: 'lastUpdated',
+                    order: 'desc')])
+  }
+
     def index() { 
       render("Hello world")
     }
